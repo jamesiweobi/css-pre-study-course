2e25fa0c865f0ddf77f8c33ccf5c569ccedae614
jamesiweobi <johnloydlegend@gmail.com>
2021-02-22 04:26:48 +0100

    Added the event listerner function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Create Button 
 const createBtn = document.createElement('button');
-// Create Button Note
+// Add label on the button
 const btnNote = document.createTextNode('Create New Note');
 
 // Add Note to the Button and add ID attribute
@@ -11,32 +11,38 @@ createBtn.setAttribute('id', 'btn');
 const element = document.querySelector('#btn_div');
 element.append(createBtn);
 
-// Adding a click listener to the button
+// Adding a click listener to the button and a function on what to do.
+createBtn.onclick = function() {
+    // Create a New article tag
+    const newArticle = document.createElement('article');
+    // Create the content of the new article h2, div_container and the <p> in the div_container
+    const newH2 = document.createElement('h2');
+    const newArticleDiv = document.createElement('div');
+    const newPar = document.createElement('p');
+    const img = document.createElement('img');
+
+    // add class attribute
+    newH2.className = 'h2-font'
+    newPar.className = 'box-2'
+    newArticleDiv.className = 'grid-container'
+    img.className = 'box-1'
+    img.id = 'img'
+    img.src = 'https://picsum.photos/536/354'
 
 
-// Create a New article tag
-const newArticle = document.createElement('article');
-// Create the content of the new article h2, div_container and the <p> in the div_container
-const newH2 = document.createElement('h2')
-const newArticleDiv = document.createElement('div')
-const newPar = document.createElement('p')
+    // Arranging the new elements in the right order
+    newArticle.appendChild(newH2)
+    newArticle.appendChild(newArticleDiv)
+    newArticleDiv.appendChild(newPar)
+    newArticleDiv.appendChild(img)
 
-// add class attribute
-newH2.className = 'h2-font'
-newPar.className = 'box-2'
-newArticleDiv.className = 'grid-container'
-
-// Arranging the new elements in the right order
-newArticle.appendChild(newH2)
-newArticle.appendChild(newArticleDiv)
-newArticleDiv.appendChild(newPar)
-
-console.log(newArticle)
-
-// Targetting and setting the new article destination
-const articleDestination = document.querySelector('main');
-const article = document.querySelector('main article')
-articleDestination.insertBefore(newArticle, article)
-
-// const newArticlePosition = document.querySelectorAll('article')
-// console.log(newArticlePosition)
+    // Targetting and setting the new article destination
+    const articleDestination = document.querySelector('main');
+    const article = document.querySelector('main article')
+    articleDestination.insertBefore(newArticle, article)
+    let h2Text = prompt('Please enter date DD/MM/YY: Title: ')
+    newH2.append(h2Text)
+    let parText = prompt('Enter your note: ')
+    newPar.append(parText)
+    newArticleDiv.append(img)
+}
